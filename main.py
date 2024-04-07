44404c4740ea3bde92bbc0a0b0e8824b6c670bf8
Sohail Nasir <166282023+SohailMSN@users.noreply.github.com>
2024-04-07 19:29:42 +0500

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+#code written by shubhank
 
 from tkinter import *
 from tkinter import ttk
@@ -10,7 +10,7 @@ import sqlite3
 from tkinter import messagebox
 now = datetime.datetime.now()
 #----------- importing sqlite for server side operations---------------------------------------------------------------------------------
-con = sqlite3.Connection('project.db')
+con = sqlite3.Connection('hm_proj.db')
 cur = con.cursor()
 cur.execute("create table if not exists hoteld(t_r number,r_r number,t_s number)")
 cur.execute("create table if not exists roomd(rn number primary key,beds number,ac varchar(10),tv varchar(10),internet varchar(10),price number(10))")
@@ -22,7 +22,8 @@ cur.execute("create table if not exists roomd(rn number primary key,beds number,
 #cur.execute("update roomd set tv='Yes' where rn = ? ",(19,))
 cur.execute("create table if not exists payments(id number primary key,dot varchar(15),time varchar(10),amt number,method varchar(10))")
 cur.execute("create table if not exists paymentsf(id number  primary key,f_name varchar,l_name varchar,c_number varchar,email varchar , r_n number ,day varchar,month varchar,year varchar,time varchar , method varchar,totalamt varchar)")
-
+#cur.execute("insert into paymentsf values(1,'Shubhank','Khare','9589861196','Shubhank7673@gmail.com',2,'1','11','2018','11:20:27 PM','Cash','3500')")
+#cur.execute("alter table paymentsf add totalamt varchar")
 con.commit()
 #cur.execute("drop table paymentsf")
 #cur.execute("insert into hoteld values(20,11,30)")
@@ -36,16 +37,12 @@ con.commit()
 sroot = Tk()
 sroot.minsize(height=516,width=1150)
 sroot.configure(bg='white')
-spath = "images/mypic.jpg"
-simg = ImageTk.PhotoImage(Image.open(spath))
-my = Label(sroot,image=simg)
-my.image = simg
-my.place(x=0,y=0)
+
 Frame(sroot,height=516,width=5,bg='black').place(x=520,y=0)
 #chilanka
-Label(sroot,text="Hotel Management System ",font='Timesnewroman 35 ',bg='white',fg='black').place(x=530,y=10)
-Label(sroot,text="Made by -",font='Timesnewroman 40 ',bg='white',fg='black').place(x=530,y=90)
-Label(sroot,text="SOHAIL NASIR AND SURESH KUMAR",font='Timesnewroman 40 ',bg='white',fg='grey').place(x=100,y=180)
+Label(sroot,text="Hotel Management System ",font='Timesnewroman 28 ',bg='white',fg='black').place(x=535,y=10)
+Label(sroot,text="Made by -",font='Timesnewroman 30 ',bg='white',fg='black').place(x=535,y=90)
+Label(sroot,text="Sohail And Suresh",font='Timesnewroman 35 ',bg='white',fg='grey').place(x=535,y=180)
 
 #Label(sroot,text="9589861196",font='Timesnewroman 40',bg='white',fg='grey').place(x=535,y=450)
 #----------- main project------------------------------------------------------------------------------------------------------------------
@@ -56,7 +53,7 @@ def mainroot():
 	root.minsize(width=1080,height=550)
 	root.maxsize(width=1080,height=550)
 	root.configure(bg='white')
-	root.title("Made by Sohail Nasir and Suresh Kumar")
+	root.title("Made by Suresh And Sohail")
 	#--------------seperator-------------------------------------------------------------------------------------------------------------------
 
 	sep = Frame(height=500,bd=1,relief='sunken',bg='white')
@@ -68,7 +65,7 @@ def mainroot():
 	#---------------top frame------------------------------------------------------------------------------------------------------------------
 
 	top_frame = Frame(root,height=70,width=1080,bg='orange')
-	path = r"C:\Users\pc\Desktop\hotel-management-system-master\images\newestbg5.jpg"
+	path = r"C:\Users\pc\Desktop\hotel-management-system-master (4)\hotel-management-system-master\images/newestbg5.jpg"
 	img = ImageTk.PhotoImage(Image.open(path))
 	label = Label(top_frame,image = img ,height=70,width=1080)
 	label.image=img
@@ -91,7 +88,7 @@ def mainroot():
 		b_frame = Frame(root,height=400,width=1080,bg='gray91')
 		b_frame.place(x=0,y=120+6+20+60+11)
 		b_frame.pack_propagate(False)
-		path = r"C:\Users\pc\Desktop\hotel-management-system-master\images\newbg6lf.jpg"
+		path = r"C:\Users\pc\Desktop\hotel-management-system-master (4)\hotel-management-system-master\images/newbg6lf.jpg"
 		img = ImageTk.PhotoImage(Image.open(path))
 		label = Label(b_frame,image = img ,height=400,width=1080)
 		label.image=img
@@ -172,7 +169,7 @@ def mainroot():
 	#-------------- Guests --------------------------------------------------------------------------------------------------------------------------
 	def staff():
 		b_frame = Frame(root,height=400,width=1080,bg='white')
-		path = r"C:\Users\pc\Desktop\hotel-management-system-master\images\newbg6lf.jpg"
+		path = r"C:\Users\pc\Desktop\hotel-management-system-master (4)\hotel-management-system-master\images/newbg6lf.jpg"
 		img = ImageTk.PhotoImage(Image.open(path))
 		label = Label(b_frame,image = img ,height=400,width=1080)
 		label.image=img
@@ -187,7 +184,7 @@ def mainroot():
 		Label(smf4,text='40',fg='cyan4',bg='white',font='msserif 50').pack(anchor='center')
 		'''
 		emp1f = Frame(b_frame)
-		path1 = "images/newman.jpg"
+		path1 = r"C:\Users\pc\Desktop\hotel-management-system-master (4)\hotel-management-system-master\images/newman.jpg"
 		img1 = ImageTk.PhotoImage(Image.open(path1))
 		emp1 = Label(emp1f,image = img1)
 		emp1.image=img1
@@ -195,14 +192,14 @@ def mainroot():
 		emp1f.place(x=0,y=0)
 		emp1inf = Frame(b_frame,bg='White',height=122,width=300)
 		Label(emp1inf,text="Manager",bg='white',font='msserif 17 bold').place(x=60,y=0)
-		Label(emp1inf,text="Sohail Nasir",bg='white',fg="Grey",font='msserif 10').place(x=60,y=37)
+		Label(emp1inf,text="Mr. Sohail",bg='white',fg="Grey",font='msserif 10').place(x=60,y=37)
 		Label(emp1inf,text="Extention : 025",bg='white',fg="Grey",font='msserif 10').place(x=60,y=59)
 		Label(emp1inf,text="Mail : Manager@hotelname.com",bg='white',fg="Grey",font='msserif 10').place(x=60,y=83)
 		emp1inf.pack_propagate(False)
 		emp1inf.place(x=117,y=1)
 
 		emp1f = Frame(b_frame)
-		path2 = r"C:\Users\pc\Desktop\hotel-management-system-master\images\receptionnew.jpg"
+		path2 = r"C:\Users\pc\Desktop\hotel-management-system-master (4)\hotel-management-system-master\images/receptionnew.jpg"
 		img2 = ImageTk.PhotoImage(Image.open(path2))
 		emp1 = Label(emp1f,image = img2)
 		emp1.image=img2
@@ -217,7 +214,7 @@ def mainroot():
 		emp1inf.place(x=767,y=2)
 
 		emp1f = Frame(b_frame)
-		path3 = r"C:\Users\pc\Desktop\hotel-management-system-master\images\fchefnew.jpg"
+		path3 = r"C:\Users\pc\Desktop\hotel-management-system-master (4)\hotel-management-system-master\images/fchefnew.jpg"
 		img3 = ImageTk.PhotoImage(Image.open(path3))
 		emp1 = Label(emp1f,image = img3)
 		emp1.image=img3
@@ -225,7 +222,7 @@ def mainroot():
 		emp1f.place(x=0,y=152)
 		emp1inf = Frame(b_frame,bg='White',height=121,width=320)
 		Label(emp1inf,text="Restaurant",bg='white',font='msserif 17 bold').place(x=72,y=0)#pack(side='top')
-		Label(emp1inf,text="Mr. Suresh Kumar (Head)",bg='white',fg="Grey",font='msserif 10').place(x=72,y=37)
+		Label(emp1inf,text="Mr. Suresh (Head)",bg='white',fg="Grey",font='msserif 10').place(x=72,y=37)
 		Label(emp1inf,text="Extention : 028",bg='white',fg="Grey",font='msserif 10').place(x=72,y=59)
 		Label(emp1inf,text="Mail : Restaurant@hotelname.com",bg='white',fg="Grey",font='msserif 10').place(x=72,y=83)	
 		emp1inf.pack_propagate(False)
@@ -233,7 +230,7 @@ def mainroot():
 		emp1inf.tkraise()
 
 		emp1f = Frame(b_frame)
-		path4 = r"C:\Users\pc\Desktop\hotel-management-system-master\images\roomservicenew.jpg"
+		path4 = r"C:\Users\pc\Desktop\hotel-management-system-master (4)\hotel-management-system-master\images/roomservicenew.jpg"
 		img4 = ImageTk.PhotoImage(Image.open(path4))
 		emp1 = Label(emp1f,image = img4)
 		emp1.image=img4
@@ -241,13 +238,46 @@ def mainroot():
 		emp1f.place(x=657,y=152)
 		emp1inf = Frame(b_frame,bg='White',height=124,width=315)
 		Label(emp1inf,text="Room Service",bg='white',font='msserif 17 bold').place(x=55,y=0)#pack(side='top')
-		Label(emp1inf,text="Ms.Ayesha (Head)",bg='white',fg="Grey",font='msserif 10').place(x=55,y=37)
+		Label(emp1inf,text="Ms. Mahnoor (Head)",bg='white',fg="Grey",font='msserif 10').place(x=55,y=37)
 		Label(emp1inf,text="Extention : 041",bg='white',fg="Grey",font='msserif 10').place(x=55,y=59)
 		Label(emp1inf,text="Mail : Roomsserv@hotelname.com",bg='white',fg="Grey",font='msserif 10').place(x=55,y=83)	
 		emp1inf.pack_propagate(False)
 		emp1inf.place(x=763,y=153)
 		#b_frame.pack_propagate(False)
-		
+		'''sidebuttons = Text(b_frame,width=1,height=19)
+		sc = Scrollbar(b_frame,command=sidebuttons.yview,width=10,bg='lightsteelblue3')
+		sidebuttons.configure(yscrollcommand=sc.set)
+		sc.pack(side='left',fill=Y)
+		sidebuttons.place(x=10,y=0)
+		b1  = Button(b_frame,font='mssherif 10', text="Room 1", bg='white',fg='cyan4',width=10)
+		b2  = Button(b_frame,font='mssherif 10', text="Room 2", bg='white',fg='cyan4',width=10)
+		b3  = Button(b_frame,font='mssherif 10', text="Room 3", bg='white',fg='cyan4',width=10)
+		b4  = Button(b_frame,font='mssherif 10', text="Room 4", bg='white',fg='cyan4',width=10)
+		b5  = Button(b_frame,font='mssherif 10', text="Room 5", bg='white',fg='cyan4',width=10)
+		b6  = Button(b_frame,font='mssherif 10', text="Room 6", bg='white',fg='cyan4',width=10)
+		b7  = Button(b_frame,font='mssherif 10', text="Room 7", bg='white',fg='cyan4',width=10)
+		b8  = Button(b_frame,font='mssherif 10', text="Room 8", bg='white',fg='cyan4',width=10)
+		b9  = Button(b_frame,font='mssherif 10', text="Room 9", bg='white',fg='cyan4',width=10)
+		b10 = Button(b_frame,font='mssherif 10', text="Room 10",bg='white',fg='cyan4',width=10)
+		sidebuttons.window_create("end",window=b1)
+		sidebuttons.insert("end","\n")
+		sidebuttons.window_create("end",window=b2)
+		sidebuttons.insert("end","\n")
+		sidebuttons.window_create("end",window=b3)
+		sidebuttons.insert("end","\n")
+		sidebuttons.window_create("end",window=b4)
+		sidebuttons.insert("end","\n")
+		sidebuttons.window_create("end",window=b5)
+		sidebuttons.insert("end","\n")
+		sidebuttons.window_create("end",window=b6)
+		sidebuttons.insert("end","\n")
+		sidebuttons.window_create("end",window=b7)
+		sidebuttons.insert("end","\n")
+		sidebuttons.window_create("end",window=b8)
+		sidebuttons.insert("end","\n")
+		sidebuttons.window_create("end",window=b9)
+		sidebuttons.insert("end","\n")
+		sidebuttons.window_create("end",window=b10)'''
 		Frame(b_frame,height=13,width=250,bg='white').place(x=410,y=2)
 		Frame(b_frame,height=13,width=250,bg='white').place(x=410,y=153)
 		#Frame(b_frame,height=180,width=13,bg='white').place(x=406,y=20)
@@ -267,7 +297,7 @@ def mainroot():
 		b_frame.place(x=0,y=120+6+20+60+11)
 		b_frame.pack_propagate(False)
 		b_frame.tkraise()
-		path = r"C:\Users\pc\Desktop\hotel-management-system-master\images\newbg6lf.jpg"
+		path = r"C:\Users\pc\Desktop\hotel-management-system-master (4)\hotel-management-system-master\images/newbg6lf.jpg"
 		img = ImageTk.PhotoImage(Image.open(path))
 		label = Label(b_frame,image = img ,height=400,width=1080)
 		label.image=img
@@ -333,7 +363,10 @@ def mainroot():
 		#b1 = Button(b_frame,font='mssherif 10', text="Room 1",bg='white',fg='cyan4',width=10,command=lambda:roomdet(1))
 	    #sidebuttons.window_create("end",window=b)                                                                                                                                           
 	    #sidebuttons.insert("end", "\n")
-		
+		'''for i in range(1,21):
+	            b = Button(b_frame,font='mssherif 10', text="Room %s" % i,bg='white',fg='cyan4',width=10,command=lambda:roomdet(i))
+	            sidebuttons.window_create("end", window=b)
+	            sidebuttons.insert("end", "\n")'''
 		sidebuttons.configure(state='disabled')
 		'''loop approach will fail when we connect database with button that's why itni mehnat'''
 		b1  = Button(b_frame,font='mssherif 10', text="Room 1", bg='white',fg='cyan4',width=10,command=lambda:roomdet(1))
@@ -402,7 +435,7 @@ def mainroot():
 
 	def payments():
 		b_frame = Frame(root,height=400,width=1080,bg='gray89')
-		path = r"C:\Users\pc\Desktop\hotel-management-system-master\images\newbg6lf.jpg"
+		path = r"C:\Users\pc\Desktop\hotel-management-system-master (4)\hotel-management-system-master\images/newbg6lf.jpg"
 		img = ImageTk.PhotoImage(Image.open(path))
 		label = Label(b_frame,image = img ,height=400,width=1080)
 		label.image=img
@@ -515,6 +548,18 @@ def mainroot():
 			l1=Label(fl1,text='Date of transaction',bg='cyan4',fg='white',font='msserif 17')
 			idd = p_id.get()
 
+			'''fl5=Frame(b_frame,height=38,width=308,bg='cyan4')
+			fl5.place(x=0,y=150)
+			l1=Label(fl5,text='Paid for room',bg='cyan4',fg='white',font='msserif 17')
+			l1.pack()
+			fl5.pack_propagate(False)
+
+			fr5=Frame(b_frame,height=38,width=1080-308,bg='white')
+			fr5.place(x=0+308,y=150)
+			l1=Label(fl1,text='Date of transaction',bg='cyan4',fg='white',font='msserif 17')
+			#l1.pack()
+			fr5.pack_propagate(False)
+			'''
 
 			#print (idd)
 	#cur.execute("create table if not exists paymentsf(id number  primary key,f_name varchar,l_name varchar,c_number varchar,email varchar , r_n number ,day varchar,month varchar,year varchar,time varchar , method varchar,totalamt varchar)")
@@ -547,15 +592,53 @@ def mainroot():
 		pinv = Button(b_frame,text='Print reciept',bg='Green',fg='white',command=pr).place(x=976,y=235)
 		#------------inner frames of bottom frame-------------------------
 
+		'''smf1 = Frame(b_frame,height=150,width=175,bg='white')
+		tr = Label(smf1,text='Total Rooms:',fg='white',bg='cyan4',width=100,height=2,font='helvetica 15')
+		tr.pack(side='top')
+		smf1.pack_propagate(False)
+		smf1.place(x=0,y=30)
+		Label(smf1,fg='cyan4',bg='white',font='msserif 50').pack(anchor='center')
+
+		smf2 = Frame(b_frame,height=150,width=175,bg='white')
+		ar = Label(smf2,text='Available Rooms:',fg='white',bg='cyan4',width=130,height=2,font='helvetica 15')
+		ar.pack(side='top')
+		smf2.pack_propagate(False)
+		smf2.place(x=180+4,y=30)
+		Label(smf2,fg='cyan4',bg='white',font='msserif 50').place(x=63,y=60)
+
+		smf3 = Frame(b_frame,height=150,width=175,bg='white')
+		tre = Label(smf3,text='Total reservations:',fg='white',bg='cyan4',width=130,height=2,font='helvetica 15')
+		tre.pack(side='top')
+		smf3.pack_propagate(False)
+		smf3.place(x=360+6,y=30)
+		Label(smf3,fg='cyan4',bg='white',font='msserif 50').pack(anchor='center')
+
+		smf4 = Frame(b_frame,height=150,width=175,bg='white')
+		tc = Label(smf4,text='Total Customers:',fg='white',bg='cyan4',width=130,height=2,font='helvetica 15')
+		tc.pack(side='top')
+		smf4.pack_propagate(False)
+		smf4.place(x=540+8,y=30)
+		Label(smf4,text='40',fg='cyan4',bg='white',font='msserif 50').pack(anchor='center')
+
+		smf5 = Frame(b_frame,height=150,width=175,bg='white')
+		ts = Label(smf5,text='Total Staff:',fg='white',bg='cyan4',width=130,height=2,font='helvetica 15')
+		ts.pack(side='top')
+		smf5.pack_propagate(False)
+		smf5.place(x=720+10,y=30)
+		Label(smf5,fg='cyan4',bg='white',font='msserif 50').pack(anchor='center')
+		#Label(b_frame,text='==================================================================================',fg='cyan4').place(x=0,y=20)
+		#b_frame.pack_propagate(False)
+	'''
+
 		nl = Label(b_frame,text='Made by Sohail And Suresh',fg='black',bg='gray91',font='msserif 8')
-		nl.place(x=930,y=290)
+		nl.place(x=955,y=310)
 		nl.tkraise()
 
 	#---------------reserve------------------------------------------------------------------------------------------------------------------------
 
 	def reserve():
 		b_frame = Frame(root,height=420,width=1080,bg='gray89')
-		path = r"C:\Users\pc\Desktop\hotel-management-system-master\images\newbg6lf.jpg"
+		path = r"C:\Users\pc\Desktop\hotel-management-system-master (4)\hotel-management-system-master\images/newbg6lf.jpg"
 		img = ImageTk.PhotoImage(Image.open(path))
 		label = Label(b_frame,image = img ,height=420,width=1080)
 		label.image=img
@@ -762,7 +845,8 @@ def mainroot():
 					Frame(payroot,height=4,width=300,bg='cyan4').place(x=0,y=39)
 					Radiobutton(payroot,text='Cash  ',bg='White',variable=pmethod,value=1,font='helvetica 15',width=5).place(x=0,y=43+10)
 					Radiobutton(payroot,text='Card   ',bg='White',variable=pmethod,value=2,font='helvetica 15',width=5).place(x=0,y=80+10)
-					
+					Radiobutton(payroot,text='UPI     ',bg='White',variable=pmethod,value=3,font='helvetica 15',width=5).place(x=0,y=115+10)
+					Radiobutton(payroot,text='Paytm ',bg='White',variable=pmethod,value=4,font='helvetica 15',width=5).place(x=0,y=150+10)
 					
 					def f():
 						if pmethod != '':
@@ -868,7 +952,7 @@ def mainroot():
 		b_frame.tkraise()
 
 		nl = Label(b_frame,text='Made by Sohail And Suresh',fg='black',bg='gray91',font='msserif 8')
-		nl.place(x=955,y=310)
+		nl.place(x=930,y=290)
 		nl.tkraise()
 	#-------------login module----------------------------------------------------------------------------------------------------------------------
 	def login():
@@ -879,32 +963,32 @@ def mainroot():
 
 	sl_frame = Frame(root,height=130,width=1080,bg='white')
 	sl_frame.place(x=0,y=70+6)
-	path = r"C:\Users\pc\Desktop\hotel-management-system-master\images\rooms.png"
+	path = r"C:\Users\pc\Desktop\hotel-management-system-master (4)\hotel-management-system-master\images/rooms.png"
 	img = ImageTk.PhotoImage(Image.open(path))
 	b1 = Button(sl_frame,image=img,text='b1',bg='white',width=180,command=rooms)
 	b1.image = img
 	b1.place(x=180,y=0)
-	path2 = r"C:\Users\pc\Desktop\hotel-management-system-master\images\hotelstatus.png"
+	path2 = r"C:\Users\pc\Desktop\hotel-management-system-master (4)\hotel-management-system-master\images/hotelstatus.png"
 	img1 = ImageTk.PhotoImage(Image.open(path2))
 	b2 = Button(sl_frame,image=img1,text='b2',bg='white',width=180,command=hotel_status)
 	b2.image = img1
 	b2.place(x=0,y=0)
-	path3=r"C:\Users\pc\Desktop\hotel-management-system-master\images\guests.png"
+	path3=r'C:\Users\pc\Desktop\hotel-management-system-master (4)\hotel-management-system-master\images/guests.png'
 	img3 = ImageTk.PhotoImage(Image.open(path3))
 	b3 = Button(sl_frame,image=img3,text='b2',bg='white',width=180,command=staff)
 	b3.image = img3
 	b3.place(x=180*4,y=0)
-	path4=r"C:\Users\pc\Desktop\hotel-management-system-master\images\payments.png"
+	path4=r'C:\Users\pc\Desktop\hotel-management-system-master (4)\hotel-management-system-master\images/payments.png'
 	img4 = ImageTk.PhotoImage(Image.open(path4))
 	b4 = Button(sl_frame,image=img4,text='b2',bg='white',width=180,command = payments)
 	b4.image = img4
 	b4.place(x=180*3,y=0)
-	path5=r'C:\Users\pc\Desktop\hotel-management-system-master\images\logout.png'
+	path5=r'C:\Users\pc\Desktop\hotel-management-system-master (4)\hotel-management-system-master\images/logout.png'
 	img5 = ImageTk.PhotoImage(Image.open(path5))
 	b5 = Button(sl_frame,image=img5,text='b2',bg='white',width=180,height=100,command=login)
 	b5.image = img5
 	b5.place(x=180*5,y=0)
-	path6=r'C:\Users\pc\Desktop\hotel-management-system-master\images\Bookroom.png'
+	path6=r'C:\Users\pc\Desktop\hotel-management-system-master (4)\hotel-management-system-master\images/Bookroom.png'
 	img6 = ImageTk.PhotoImage(Image.open(path6))
 	b6 = Button(sl_frame,image=img6,text='b2',bg='white',width=180,height=100,command=reserve)
 	b6.image = img6
